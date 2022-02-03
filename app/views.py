402bfd98df4c65a28a2ab5bccc124c746c89ecb0
rohith-rdx <rohithdrizex123@gmail.com>
2022-02-03 02:34:12 -0800

    dashboard page created

--- app/views.py
+++ app/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
 
+def task(request):
+    return render(request,'task.html')
